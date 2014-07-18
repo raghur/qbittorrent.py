93867de1ab405817acd9850ed4778bb5773377f8
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2014-07-18 23:39:13 +0530

    more tests for main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -21,3 +21,54 @@ def test_should_run_list_command_with_explicit_filter(mock):
     mock.side_effect = side_effect
     main(["list", "--state", "uploading"])
     mock.assert_called_with(ANY)
+
+
+@patch('qbittorrent.main.pauseTorrentsCommand', autospec=True)
+def test_should_run_pause_by_state(mock):
+    def side_effect(a):
+        assert_that(a.state).is_equal_to("stalledUP")
+        return DEFAULT
+    mock.side_effect = side_effect
+    main("pause -s stalledUP".split(" "))
+    mock.assert_called_with(ANY)
+
+
+@patch('qbittorrent.main.pauseTorrentsCommand', autospec=True)
+def test_should_run_pause_by_torrentlist(mock):
+    def side_effect(a):
+        assert_that(a.torrents).is_equal_to(["1", "2", "3"])
+        return DEFAULT
+    mock.side_effect = side_effect
+    main("pause -t 1 2 3".split(" "))
+    mock.assert_called_with(ANY)
+
+
+@patch('qbittorrent.main.resumeTorrentsCommand', autospec=True)
+def test_should_resume_by_state(mock):
+    def side_effect(a):
+        assert_that(a.state).is_equal_to("stalledUP")
+        return DEFAULT
+    mock.side_effect = side_effect
+    main("resume -s stalledUP".split(" "))
+    mock.assert_called_with(ANY)
+
+
+@patch('qbittorrent.main.resumeTorrentsCommand', autospec=True)
+def test_should_resume_by_torrentlist(mock):
+    def side_effect(a):
+        assert_that(a.torrents).is_equal_to(["1", "2", "3"])
+        return DEFAULT
+    mock.side_effect = side_effect
+    main("resume -t 1 2 3".split(" "))
+    mock.assert_called_with(ANY)
+
+
+@patch('qbittorrent.main.manageQueueCommand', autospec=True)
+def test_should_manage_queue(mock):
+    def side_effect(a):
+        assert_that(a.torrent).is_equal_to(["1"])
+        assert_that(a.action).is_equal_to("up")
+        return DEFAULT
+    mock.side_effect = side_effect
+    main("queue -a up 1".split(" "))
+    mock.assert_called_with(ANY)
