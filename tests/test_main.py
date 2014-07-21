0b96e5f0017c0bf6a610d712b1cf58d2ad522229
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2014-07-21 15:10:32 +0530

    ensure tests exercise calls to QBittorrent class

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,6 +13,14 @@ def test_should_run_list_command(mock):
     mock.assert_called_with(ANY)
 
 
+@patch('qbittorrent.qbittorrent.QBitTorrent', autospec=True)
+def test_listTorrents_should_call_qbittorrent_with_None(mock):
+    instance = mock.return_value
+    instance.getTorrents.return_value = []
+    main(["list"])
+    instance.getTorrents.assert_called_with(None)
+
+
 @patch('qbittorrent.main.listTorrentsCommand', autospec=True)
 def test_should_run_list_command_with_explicit_filter(mock):
     def side_effect(a):
@@ -21,6 +29,22 @@ def test_should_run_list_command_with_explicit_filter(mock):
     mock.side_effect = side_effect
     main(["list", "uploading"])
     mock.assert_called_with(ANY)
+
+
+@patch('qbittorrent.qbittorrent.QBitTorrent', autospec=True)
+def test_pauseTorrentsCommand_should_call_with_state_array(mock):
+    instance = mock.return_value
+    instance.pauseTorrents.return_value = []
+    main("pause -s stalledUP".split(" "))
+    instance.pauseTorrents.assert_called_with(state=["stalledUP"])
+
+
+@patch('qbittorrent.qbittorrent.QBitTorrent', autospec=True)
+def test_pauseTorrentsCommand_should_call_with_torrent_list(mock):
+    instance = mock.return_value
+    instance.pauseTorrents.return_value = []
+    main("pause -t 1 2 3 4".split(" "))
+    instance.pauseTorrents.assert_called_with(hashes="1 2 3 4".split(" "))
 
 
 @patch('qbittorrent.main.pauseTorrentsCommand', autospec=True)
@@ -51,6 +75,22 @@ def test_should_resume_by_state(mock):
     mock.side_effect = side_effect
     main("resume -s stalledUP".split(" "))
     mock.assert_called_with(ANY)
+
+
+@patch('qbittorrent.qbittorrent.QBitTorrent', autospec=True)
+def test_resumeTorrentsCommand_should_call_with_state_array(mock):
+    instance = mock.return_value
+    instance.resumeTorrents.return_value = []
+    main("resume -s stalledUP".split(" "))
+    instance.resumeTorrents.assert_called_with(state=["stalledUP"])
+
+
+@patch('qbittorrent.qbittorrent.QBitTorrent', autospec=True)
+def test_resumeTorrentsCommand_should_call_with_torrent_list(mock):
+    instance = mock.return_value
+    instance.resumeTorrents.return_value = []
+    main("resume -t 1 2 3 4".split(" "))
+    instance.resumeTorrents.assert_called_with(hashes="1 2 3 4".split(" "))
 
 
 @patch('qbittorrent.main.resumeTorrentsCommand', autospec=True)
